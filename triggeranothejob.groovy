4a1239c5d7d757f4295e4970eb933a24b1916fe9
acld accnt01 <acldaccnt01@gmail.com>
2022-06-06 13:12:48 +0530

    update trigger another job file

--- triggeranothejob.groovy
+++ triggeranothejob.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.Jenkins;
-def target = binding.variables["target"]
+def target = build.buildVariableResolver.resolve["target"];
 def svr = Jenkins.instance;
-def job = svr.getJob(target)
+def job = svr.getJob(target);
 def sched = job.scheduleBuild2(0);
 sched.get();
